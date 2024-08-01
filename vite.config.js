af3ade56aeef32dff59797454becfccb23e5c292
gerardocrr <gerardo2551.cr@gmail.com>
2024-07-31 23:25:53 -0600

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+  };
+});
