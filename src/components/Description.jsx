0c363d8fa612e7b38acde0a45d8dc855c0e4f6ec
gerardocrr <gerardo2551.cr@gmail.com>
2024-08-02 23:38:41 -0600

    update description component

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Dropzone } from "./Dropzone";
-import { FeedbackAI } from "./FeedbackAI";
 
 export function Description() {
   const [data, setData] = useState({});
@@ -20,33 +18,23 @@ export function Description() {
   }, []);
 
   return (
-    <div className="grid grid-cols-3 gap-8 mx-auto p-5 h-screen">
-      <div className="flex flex-col bg-gray-400 rounded-lg p-2 overflow-y-scroll">
-        <h1>Description</h1>
-        {data.title ? (
-          <div>
-            <h1>{data.title}</h1>
-            <p>{data.company}</p>
-            <p>{data.location}</p>
-            <br />
-            <p>{data.jobCriteria.employmentType}</p>
-            <p>{data.jobCriteria.industries}</p>
-            <p>{data.jobCriteria.jobFunction}</p>
-            <p>{data.jobCriteria.seniorityLevel}</p>
-            <br />
-            <p>{data.description}</p>
-          </div>
-        ) : (
-          <p>Loading...</p>
-        )}
-      </div>
-      <div className="flex flex-col bg-gray-400 rounded-lg p-2">
-        <h1>CV</h1>
-        <Dropzone />
-      </div>
-      <div className="flex flex-col bg-gray-400 rounded-lg p-2">
-        <FeedbackAI />
-      </div>
+    <div>
+      {data.title ? (
+        <div>
+          <h1>{data.title}</h1>
+          <p>{data.company}</p>
+          <p>{data.location}</p>
+          <br />
+          <p>{data.jobCriteria.employmentType}</p>
+          <p>{data.jobCriteria.industries}</p>
+          <p>{data.jobCriteria.jobFunction}</p>
+          <p>{data.jobCriteria.seniorityLevel}</p>
+          <br />
+          <p>{data.description}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
