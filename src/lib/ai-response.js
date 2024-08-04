0115037aa1ca90ca21a37ed1fad2dc7ef1e13c60
gerardocrr <gerardo2551.cr@gmail.com>
2024-08-03 19:05:19 -0600

    update prompt to ai-response

--- src/lib/ai-response.js
+++ src/lib/ai-response.js
@@ -6,8 +6,22 @@ const perplexity = createOpenAI({
   baseURL: "https://api.perplexity.ai",
 });
 
-export async function getFeedback() {
-  const prompt = "Dime un cuento muy corto sobre la amistad";
+export async function getFeedback(dataCV, dataJob) {
+  const prompt = `Analiza el siguiente CV y la descripción del trabajo proporcionada. Evalúa la aptitud del candidato para el 
+  puesto y proporciona recomendaciones específicas sobre áreas de mejora o habilidades adicionales que el candidato podría necesitar para 
+  aumentar sus posibilidades de ser seleccionado.
+  Datos del candidato:
+  ${dataCV}
+
+  Descripción del trabajo:
+  ${dataJob}
+
+  Tu respuesta debe incluir:
+
+  1. Un análisis de la compatibilidad del candidato con los requisitos del trabajo.
+  2. Las fortalezas del candidato en relación al puesto.
+  3. Las áreas donde el candidato podría mejorar o adquirir más habilidades.
+  4. Consejos específicos para mejorar el CV del candidato en función de la descripción del trabajo.`;
 
   const { text } = await generateText({
     model: perplexity("llama-3-sonar-large-32k-online"),
