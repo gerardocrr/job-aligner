9a0f3645155c104964f99c4e3dca6d0e45b8e05b
gerardocrr <gerardo2551.cr@gmail.com>
2024-08-04 23:07:44 -0600

    added file css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { JobSearch } from "./pages/JobSearch";
 import { MainPage } from "./pages/MainPage";
+import "./App.css";
 
 function App() {
   return (
