840837476f93535bc4f6af83ec0c6cc5f9bb6d27
gerardocrr <gerardo2551.cr@gmail.com>
2024-08-04 23:07:23 -0600

    update main page ui

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -12,17 +12,17 @@ export function MainPage() {
   const dataJobText = `Title: ${dataJob.title}. Seniority level: ${dataJob.jobCriteria?.seniorityLevel}. Employment type: ${dataJob.jobCriteria?.employmentType}. Job function: ${dataJob.jobCriteria?.jobFunction}. Industries: ${dataJob.jobCriteria?.industries}. Description: ${dataJob.description}.`;
 
   return (
-    <div className="grid grid-cols-3 gap-8 mx-auto p-5 h-screen">
+    <div className="grid grid-cols-3 gap-8 mx-5 p-2 h-screen">
       <div className="col-span-3 h-5">
         <Header />
       </div>
-      <div className="flex flex-col bg-gray-400 rounded-lg p-2 overflow-y-scroll">
+      <div className="flex flex-col bg-blue-400 bg-opacity-40 rounded-lg p-2 overflow-y-scroll">
         <DescriptionJob dataJob={dataJob} setDataJob={setDataJob} />
       </div>
-      <div className="flex flex-col bg-gray-400 rounded-lg p-2">
+      <div className="flex flex-col bg-blue-400 bg-opacity-40 rounded-lg p-2">
         <Dropzone file={file} setFile={setFile} setDataCV={setDataCV} />
       </div>
-      <div className="flex flex-col bg-gray-400 rounded-lg p-2">
+      <div className="flex flex-col bg-blue-400 bg-opacity-40 rounded-lg p-2">
         <FeedbackAI dataCV={dataCV} dataJob={dataJobText} />
       </div>
     </div>
