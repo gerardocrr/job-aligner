79f220a4b57439f07bd7948acd74f7da36414d6b
gerardocrr <gerardo2551.cr@gmail.com>
2024-08-05 23:48:19 -0600

    update description component

--- src/components/DescriptionJob.jsx
+++ src/components/DescriptionJob.jsx
@@ -1,39 +1,21 @@
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
-
-export function DescriptionJob({ dataJob, setDataJob }) {
-  const params = useParams();
-
-  useEffect(() => {
-    const fetchDatos = async () => {
-      const response = await fetch(
-        `https://api-linkedin.vercel.app/api/jobs/${params.id}`
-      );
-      const data = await response.json();
-      setDataJob(data);
-    };
-    fetchDatos();
-  }, []);
-
+export function DescriptionJob({ dataJob }) {
   return (
     <div>
-      <h1>Description</h1>
-      {dataJob.title ? (
-        <div>
-          <h1>{dataJob.title}</h1>
-          <p>{dataJob.company}</p>
-          <p>{dataJob.location}</p>
-          <br />
-          <p>{dataJob.jobCriteria.employmentType}</p>
-          <p>{dataJob.jobCriteria.industries}</p>
-          <p>{dataJob.jobCriteria.jobFunction}</p>
-          <p>{dataJob.jobCriteria.seniorityLevel}</p>
-          <br />
-          <p>{dataJob.description}</p>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+      <h1 className="font-bold mb-5">Descripción del empleo</h1>
+      <div>
+        <p>Título: {dataJob.title}</p>
+        <p>Empresa: {dataJob.company}</p>
+        <p>Ubicaciónn: {dataJob.location}</p>
+        <br />
+        <p>Criterios del empleo:</p>
+        <li>Tipo de empleo: {dataJob.jobCriteria.employmentType}</li>
+        <li>Industria: {dataJob.jobCriteria.industries}</li>
+        <li>Funciones del empleo: {dataJob.jobCriteria.jobFunction}</li>
+        <li>Seniority: {dataJob.jobCriteria.seniorityLevel}</li>
+        <br />
+        <p>Descripción:</p>
+        <p>{dataJob.description}</p>
+      </div>
     </div>
   );
 }
