10e5cbecb965f9be2beaac154540e51d71d0db32
gerardocrr <gerardo2551.cr@gmail.com>
2024-10-27 23:57:55 -0600

    Refactor vite.config.js to remove unused import and improve code readability

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
     plugins: [react()],
